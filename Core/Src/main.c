1c3fed2f41dba0f92a784d677b04d2d863a0a053
Starfir <starfir2@163.com>
2025-10-07 19:11:16 +0800

    配置定时器等

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,12 +19,17 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "can.h"
+#include "i2c.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "oled.h"
+#include "font.h"
+#include "motor.h"
+#include "pid.h"
 
 /* USER CODE END Includes */
 
@@ -92,6 +97,12 @@ int main(void)
   MX_CAN1_Init();
   MX_TIM1_Init();
   MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_TIM5_Init();
+  MX_TIM8_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
